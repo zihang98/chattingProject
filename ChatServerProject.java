b1d6f42a5e786e3c2e5a5b0044c40b7cfc1f698b
zihang98 <81303872+zihang98@users.noreply.github.com>
2024-04-29 14:01:48 +0900

    Add files via upload

--- ChatServerProject.java
+++ ChatServerProject.java
@@ -4,6 +4,8 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class ChatServerProject {
@@ -12,10 +14,12 @@ public class ChatServerProject {
             System.out.println("서버가 준비됨");
 
             Map<String, PrintWriter> chatClients = new HashMap<>();
+            Map<Integer, Room> rooms = new HashMap<>();
+            List<Integer> roomNumbers = new LinkedList<>();
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                ChatThreadProject chatThread = new ChatThreadProject(clientSocket, chatClients);
+                ChatThreadProject chatThread = new ChatThreadProject(clientSocket, chatClients, rooms, roomNumbers);
                 chatThread.start();
             }
         } catch (Exception e) {
--- ChatThreadProject.java
+++ ChatThreadProject.java
@@ -1,11 +1,10 @@
 package com.example.day0426;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 class Room {
     public List<String> users = new LinkedList<>();
@@ -32,22 +31,34 @@ public class ChatThreadProject extends Thread {
     private Socket socket;
     private String nickname;
     private Map<String, PrintWriter> chatClients;
-    private Map<Integer, Room> rooms = new HashMap<>();
-    private List<Integer> roomNumbers = new LinkedList<>();
+    private Map<Integer, Room> rooms;
+    private List<Integer> roomNumbers;
     int myRoomNumber = 0; // 0이면 방 밖에 있다는 뜻. 번호가 있으면 그 번호의 방안에 있다는 뜻.
     BufferedReader in;
     PrintWriter out;
+    File chatLog;
 
-    public ChatThreadProject(Socket socket, Map<String, PrintWriter> chatClients) {
+    public ChatThreadProject(Socket socket, Map<String, PrintWriter> chatClients, Map<Integer, Room> rooms, List<Integer> roomNumbers) {
         this.socket = socket;
         this.chatClients = chatClients;
+        this.rooms = rooms;
+        this.roomNumbers = roomNumbers;
 
         try {
             out = new PrintWriter(socket.getOutputStream(), true);
             in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-            nickname = in.readLine();
-            //speakInRoom(nickname + "님이 입장");
+            // 닉네임 중복 검사
+            while (true) {
+                nickname = in.readLine();
+                if (chatClients.containsKey(nickname)) {
+                    out.println("이미 있는 닉네임입니다. 다른 닉네임을 입력해주세요.");
+                    out.println("Enter your nickname: ");
+                } else {
+                    break;
+                }
+            }
+
             System.out.println("새로운 사용자의 id : " + nickname);
 
             // put 작업이 동시에 일어날 수도 있음
@@ -55,9 +66,18 @@ public class ChatThreadProject extends Thread {
                 chatClients.put(nickname, out);
             }
 
+            out.println("/help로 명령어들을 볼 수 있습니다.");
+
+            this.chatLog = new File("src/com/example/day0426/" + nickname + ".log");
+
+            if (!chatLog.exists()) {
+                chatLog.createNewFile();
+            }
+            
         } catch (Exception e) {
             System.out.println(e);
         }
+
     }
 
     @Override
@@ -65,10 +85,25 @@ public class ChatThreadProject extends Thread {
         String message = null;
         try {
             while ((message = in.readLine()) != null) {
+
                 String[] messageArray = message.split(" ", 3);
+
+                // help : 명령어들 보여주기
+                if ("/help".equalsIgnoreCase(message)) {
+                    out.println("/bye : 종료하기\n" +
+                            "/create : 방 생성하기\n" +
+                            "/list : 방 목록 보기\n" +
+                            "/join 방번호 : 방에 들어가기\n" +
+                            "/exit : 방 나가기\n" +
+                            "/users : 서버에 있는 유저들을 보여주기\n" +
+                            "/roomusers : 방에 있는 유저들을 보여주기\n" +
+                            "/w 닉네임 메시지 : 해당 유저에게만 메시지 보내기");
+                    continue;
+                }
 
                 // bye : 종료
                 if ("/bye".equalsIgnoreCase(message)) {
+                    out.println("종료합니다.");
                     break;
                 }
 
@@ -100,15 +135,27 @@ public class ChatThreadProject extends Thread {
                 // exit : 방 나가기. 방을 나가서 방에 있는 유저의 수가 0이 되면 방을 삭제함
                 if ("/exit".equalsIgnoreCase(message)) {
                     if (myRoomNumber != 0) {
-                        rooms.get(myRoomNumber).leave(nickname);
+                        synchronized (rooms) {
+                            rooms.get(myRoomNumber).leave(nickname);
+                        }
+
+                        // 방에 있는 유저들에게 나갔다고 알리기
+                        for (String nick : rooms.get(myRoomNumber).users) {
+                            chatClients.get(nick).println(nickname + "님이 나갔습니다.");
+                        }
+
+                        out.println("로비로 나왔습니다.");
 
                         if (rooms.get(myRoomNumber).users.isEmpty()) {
-                            rooms.remove(myRoomNumber);
-                            roomNumbers.remove(Integer.valueOf(myRoomNumber)); // myRoomNumber를 인덱스가 아닌 값으로
+                            synchronized (rooms) {
+                                rooms.remove(myRoomNumber);
+                            }
+                            synchronized (roomNumbers) {
+                                roomNumbers.remove(Integer.valueOf(myRoomNumber));
+                            }// myRoomNumber를 인덱스가 아닌 값으로 인식
                         }
 
                         myRoomNumber = 0;
-                        out.println("로비로 나왔습니다.");
                     } else {
                         out.println("방 안이 아닙니다.");
                     }
@@ -136,7 +183,6 @@ public class ChatThreadProject extends Thread {
                 // w 닉네임 메시지 : 귓속말 기능. 닉네임에게만 메시지를 보낸다.
                 if ("/w".equalsIgnoreCase(messageArray[0])) {
                     if (chatClients.containsKey(messageArray[1])) {
-                        System.out.println("messageArray : " + Arrays.toString(messageArray));
                         chatClients.get(messageArray[1]).println(nickname + "의 귓말 : " + messageArray[2]);
                     } else {
                         chatClients.get(nickname).println("접속해있지 않은 사용자입니다.");
@@ -152,22 +198,7 @@ public class ChatThreadProject extends Thread {
                     out.println("로비에서는 채팅을 할 수 없습니다.");
                     continue;
                 }
-
-
-//                // 귓말
-//                if ("/to".equalsIgnoreCase(messageArray[0])) {
-//                    if (chatClients.containsKey(messageArray[1])) {
-//                        System.out.println("messageArray : " + Arrays.toString(messageArray));
-//                        chatClients.get(messageArray[1]).println(nickname + "의 귓말 : " + messageArray[2]);
-//                    } else {
-//                        chatClients.get(nickname).println("접속해있지 않은 사용자입니다");
-//                    }
-//                    continue;
-//                }
-
-//                speakInRoom(nickname + " : " + message);
             }
-
         } catch (Exception e) {
             System.out.println(e);
             e.printStackTrace();
@@ -198,7 +229,7 @@ public class ChatThreadProject extends Thread {
 
     public void speakInRoom(String message) {
         for (String nick : rooms.get(myRoomNumber).users) {
-            chatClients.get(nick).println(message);
+            chatClients.get(nick).println(nickname + " : " + message);
         }
     }
 
@@ -232,13 +263,11 @@ public class ChatThreadProject extends Thread {
             rooms.get(roomNumber).enter(nickname);
             myRoomNumber = roomNumber;
             out.println(myRoomNumber + "번 방에 들어왔습니다.");
+            for (String nick : rooms.get(myRoomNumber).users) {
+                chatClients.get(nick).println(nickname + "님이 들어왔습니다. ");
+            }
         } else {
             out.println("존재하지 않는 방입니다.");
         }
     }
-
-//    public void exitRoom() {
-//
-//    }
-
 }
